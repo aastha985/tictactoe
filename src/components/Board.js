cd071f01102f02cc4af297086f348dfce016f327
aastha985 <55681187+aastha985@users.noreply.github.com>
2020-06-12 23:31:26 +0530

    created board

---
+++ src/components/Board.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Square from './Square';
+
+export default function Board(){
+    return (
+        <div>
+            <div className="board-row">
+                <Square/>
+                <Square/>
+                <Square/>
+            </div>
+            <div className="board-row">
+                <Square/>
+                <Square/>
+                <Square/>
+            </div>
+            <div className="board-row">
+                <Square/>
+                <Square/>
+                <Square/>
+            </div>
+        </div>
+    )
+}
+
