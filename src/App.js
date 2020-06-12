50a773c46e57c83bc41ac2eed3215af4c1638d7a
aastha985 <55681187+aastha985@users.noreply.github.com>
2020-06-12 23:41:50 +0530

    changes in App

--- src/App.js
+++ src/App.js
@@ -1,50 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Game from './components/Game';
 
 class App extends React.Component{
   render(){
     return(
-      // <h1>I am a class based component</h1>
-      <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This is a react project.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Game/>
+      
     ) 
   }
 }
-
-// function App() {
-//   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       This is a react project.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-//   );
-// }
 
 export default App;
