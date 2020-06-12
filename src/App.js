d1b1eaa6e236c091dbe4444dcfff8b5233ba2acf
aastha985 <55681187+aastha985@users.noreply.github.com>
2020-06-12 22:35:08 +0530

    class based & functional based components

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+class App extends React.Component{
+  render(){
+    return(
+      // <h1>I am a class based component</h1>
+      <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is a react project.
         </p>
         <a
           className="App-link"
@@ -20,7 +22,29 @@ function App() {
         </a>
       </header>
     </div>
-  );
+    ) 
+  }
 }
+
+// function App() {
+//   return (
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       This is a react project.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+//   );
+// }
 
 export default App;
